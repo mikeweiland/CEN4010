0debfc43cc9486ab68c9fa31dd35da01170b917c
Eric Rado <Eric@Erics-MacBook-Air.local>
2017-07-17 19:10:19 -0400

    Added Tech Valley Times model, user can update their card, address and personal information.

--- bookstore/accounts/forms.py
+++ bookstore/accounts/forms.py
@@ -25,6 +25,36 @@ class EditUserProfileForm(ModelForm):
     nickname = forms.CharField(label='Nickname')
     email_address = forms.EmailField(label="Email Address")
 
+    def clean_nickname(self):
+        nickname = self.cleaned_data['nickname']
+
+        # check if nickname submitted is the same as the current user online
+        if nickname == self.instance.nickname:
+            return nickname
+
+        # username is different from previous, check if it already exists
+        if User.objects.get(nickname=nickname):
+            raise forms.ValidationError('Nickname already exists.')
+
+        return nickname
+
+    def clean_email_address(self):
+        email_address = self.cleaned_data['email_address']
+        print('The email address : ' + email_address)
+        print('The email address : ' + self.instance.email_address)
+
+        # check if email address submitted is the same as the current user online
+        if email_address == self.instance.email_address:
+            return email_address
+
+        # email address is different from previous, check if it already exits
+        try:
+            User.objects.exclude(pk=self.instance.user_id).get(email_address=email_address)
+        except User.DoesNotExist:
+            return email_address
+
+        raise forms.ValidationError('Email Address already exists')
+
     class Meta:
         model = User
         fields = ['first_name', 'last_name','nickname','email_address']
@@ -35,6 +65,20 @@ class AddressForm(ModelForm):
     city = forms.CharField(max_length=50, label="City")
     state = forms.CharField(max_length=2, label="State")
     zip_code = forms.CharField(max_length=5, label="Zip Code")
+
+    def clean_zip_code(self):
+        zip_code = self.cleaned_data['zip_code']
+        if not zip_code.isdigit():
+            raise forms.ValidationError('Zip Code can only contains numbers.')
+
+        return zip_code
+
+    def clean_state(self):
+        state = self.cleaned_data['state']
+        if not state.isalpha():
+            raise forms.ValidationError('State can only contain letters.')
+
+        return state
 
     class Meta:
         model = Address
--- bookstore/products/admin.py
+++ bookstore/products/admin.py
@@ -6,3 +6,4 @@ from . import models
 admin.site.register(models.Book)
 admin.site.register(models.Author)
 admin.site.register(models.Review)
+admin.site.register(models.TechValleyTimes)
